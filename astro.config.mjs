7574df9ee0689a23fb2a412a65f197f387bdd525
exa04 <223230@proton.me>
2024-08-19 23:52:58 +0200

    Added tailwind and a lot of work on the index.mdx page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,45 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
       title: "React-Plug",
       logo: {
-        light: "./src/assets/logo.svg",
-        dark: "./src/assets/logo_dark.svg",
-        replacesTitle: true,
+        light: "./src/assets/logo_circle_light.svg",
+        dark: "./src/assets/logo_circle_dark.svg",
       },
-      customCss: ["./src/styles/custom.css", "./src/fonts/inter.css"],
+      customCss: ["./src/styles/custom.css", "./src/fonts/inter.css", './src/styles/tailwind.css'],
       social: {
-        github: "https://github.com/exa04/react_plug",
+        github: "https://github.com/exa04/react_plug"
       },
       components: {
-        SocialIcons: "./src/components/HeaderLinks.astro",
+        SocialIcons: "./src/components/HeaderLinks.astro"
       },
-      sidebar: [
-        {
-          label: "Guides",
-          items: [
-            { label: "Quick Start", slug: "guides/quick-start" },
-            { label: "Getting Started", slug: "guides/getting-started" },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
-      ],
+      sidebar: [{
+        label: "Guides",
+        items: [{
+          label: "Quick Start",
+          slug: "guides/quick-start"
+        }, {
+          label: "Getting Started",
+          slug: "guides/getting-started"
+        }]
+      }, {
+        label: "Reference",
+        autogenerate: {
+          directory: "reference"
+        }
+      }],
       tableOfContents: {
-        maxHeadingLevel: 4,
-      },
+        maxHeadingLevel: 4
+      }
     }),
-  ],
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ]
 });
